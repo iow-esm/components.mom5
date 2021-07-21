486daad75625992d823194b8adbc37eb91913563
Sven Karsten <sven.karsten@io-warnemuende.de>
2021-07-21 15:18:43 +0200

    Build script can create directories on target.
    Sync additional files necessary for building.

--- build.sh
+++ build.sh
@@ -7,11 +7,19 @@ source ../../local_scripts/identify_target.sh $target $debug $fast
 # component to build
 component="MOM5"
 
+# create component's directory (if not existing)
+echo ssh -t "${user_at_dest}" "mkdir -p ${dest_folder}/components/${component}"
+ssh -t "${user_at_dest}" "mkdir -p ${dest_folder}/components/${component}"
+
 # deploy the code from local source
 echo rsync -r -i -u src ${dest}/components/${component}/.
+echo rsync -r -i -u bin ${dest}/components/${component}/.
+echo rsync -r -i -u exp ${dest}/components/${component}/.
 echo rsync -i -u start_build_${target}.sh ${dest}/components/${component}/
 echo rsync -i -u build_${target}.sh ${dest}/components/${component}/
 rsync -r -i -u src ${dest}/components/${component}/.
+rsync -r -i -u bin ${dest}/components/${component}/.
+rsync -r -i -u exp ${dest}/components/${component}/.
 rsync -i -u start_build_${target}.sh ${dest}/components/${component}/
 rsync -i -u build_${target}.sh ${dest}/components/${component}/
 
